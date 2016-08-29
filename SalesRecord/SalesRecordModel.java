a2dd0ff961bb69c8199278bc8da664bf4b4322d1
jmac92 <justin_maciburko@hotmail.com>
2016-08-29 17:02:24 +1000

    Adds initial method structures for adding, editing, displaying and deleting sales records

---
+++ SalesRecord/SalesRecordModel.java
@@ -0,0 +1,26 @@
+public class SalesRecordModel {
+	
+	public String addSaleItem (String name, String date, String qty)
+	{
+		String result = "INSERT INTO salesrecord (ItemName, SaleDate, SaleQuantity) VALUES ('name', 'date', 'qty')";
+		return result;
+	}
+
+	public String editSaleItem (String id, String name, String date, String qty)
+	{
+		String result = "UPDATE salesrecord SET ItemName = 'name', SaleDate = 'date', SaleQuantity = 'qty' WHERE SaleID = 'id'";
+		return result;
+	}
+	
+	public String displaySaleItem (String id)
+	{
+		String result = "SELECT * FROM salesrecord WHERE SaleID = 'id'";
+		return result;
+	}
+	
+	public String deleteSaleItem (String id)
+	{
+		String result = "DELETE FROM salesrecord WHERE SaleID = 'id'";
+		return result;
+	}
+}
