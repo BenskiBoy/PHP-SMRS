342ac8686d3a91e526143b3d7d7d9bd7ea25b1e3
Thomas Cheah <Thomas Cheah>
2016-09-05 15:52:50 +1000

    addition of stock code

---
+++ StockItem/StockItem.java
@@ -0,0 +1,59 @@
+package stockitem;
+public class StockItem {
+	private int stockId;
+	private String name;
+	private String manufacturer;
+	private String description;
+	private double listPrice;
+	private int quantity;
+	public StockItem(int id, String nam, String man, String desc, double price, int quan){
+		this.stockId = id;
+		this.name = nam;
+		this.manufacturer = man;
+		this.description = desc;
+		this.listPrice = price;
+		this.quantity = quan;
+	}
+	public void setStockId(int value){
+		this.stockId = value;
+	}
+	public int getStockId(){
+		return this.stockId;
+	}
+	public void setName(String str){
+		this.name = str;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setManufacturer(String man){
+		this.manufacturer = man;
+	}
+	public String getManufacturer(){
+		return this.manufacturer;
+	}
+	public void setDescription(String desc){
+		this.description = desc;
+	}
+	public String getDescription(){
+		return this.description;
+	}
+	public void setListPrice(double price){
+		this.listPrice = price;
+	}
+	public double getListPrice(){
+		return this.listPrice;
+	}
+	public void setQuantity(int amount){
+		this.quantity = amount;
+	}
+	public int getQuantity(){
+		return this.quantity;
+	}
+	public void incrementQuantity(int amount){
+		this.quantity += amount;
+	}
+	public void decrementQuantity(int amount){
+		this.quantity += amount;
+	}
+}
