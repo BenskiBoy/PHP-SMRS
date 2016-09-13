988ab69516c544da0d4e8e7de47610e50236ebaa
Jmac92 <justin_maciburko@hotmail.com>
2016-09-13 22:01:21 +1000

    Fixes variable passing in sql strings.

--- SalesRecord/SalesRecordModel.java
+++ SalesRecord/SalesRecordModel.java
@@ -2,25 +2,25 @@ public class SalesRecordModel {
 	
 	public String addSaleItem (String name, String date, String qty)
 	{
-		String result = "INSERT INTO salesrecord (ItemName, SaleDate, SaleQuantity) VALUES ('name', 'date', 'qty')";
+		String result = "INSERT INTO salesrecord (ItemName, SaleDate, SaleQuantity) VALUES ('"+ name +"', '"+ date +"', '"+ qty +"')";
 		return result;
 	}
 
 	public String editSaleItem (String id, String name, String date, String qty)
 	{
-		String result = "UPDATE salesrecord SET ItemName = 'name', SaleDate = 'date', SaleQuantity = 'qty' WHERE SaleID = 'id'";
+		String result = "UPDATE salesrecord SET ItemName = '"+ name +"', SaleDate = '"+ date +"', SaleQuantity = '"+ qty +"' WHERE SaleID = '"+ id +"'";
 		return result;
 	}
 	
 	public String displaySaleItem (String id)
 	{
-		String result = "SELECT * FROM salesrecord WHERE SaleID = 'id'";
+		String result = "SELECT * FROM salesrecord WHERE SaleID = '"+ id +"'";
 		return result;
 	}
 	
 	public String deleteSaleItem (String id)
 	{
-		String result = "DELETE FROM salesrecord WHERE SaleID = 'id'";
+		String result = "DELETE FROM salesrecord WHERE SaleID = '"+ id +"'";
 		return result;
 	}
 }
